0509c6e6c17148759d9ada11ec8d958fca9eb93f
joshah-moors <joshah-moors@users.noreply.github.com>
2020-06-18 14:58:22 +0000

    deploy: 29691f3ff01faf3580669ef7d1e2a5f817564049

---
+++ precache-manifest.b0966b3ecc0887b785604b782ae65b9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e940a3e5549dfd89d5a9efd96c80f27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d79f440eaf0d50fc8a7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e80687bcda116c6152a0",
+    "url": "/static/js/2.6766b688.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6766b688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d79f440eaf0d50fc8a7",
+    "url": "/static/js/main.94147ae4.chunk.js"
+  },
+  {
+    "revision": "1bb77652b8c84ca4952f",
+    "url": "/static/js/runtime-main.da5fb971.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
