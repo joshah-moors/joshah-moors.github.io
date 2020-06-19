d706bd866baab3e72e69c72deaf89a7a8e4891bb
Joshah <joshah.moors@utexas.edu>
2020-06-19 08:45:07 -0500

    Icons on Homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
-import index from './index.jpg';
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
 
+import index from './index.jpg';
 
 
 
@@ -11,8 +13,19 @@ class Home extends Component {
       <div className="home">
         <img src={index} className="home-pic" alt="logo" />
         <span className="home-name">Joshah Moors</span>
-        <p className="home-linked">
+        {/*<p className="home-linked">
           Check out my skills and hit me up on <a className="home-a" href="https://www.linkedin.com/in/joshahmoors/">LinkedIn</a>
+        </p> */}
+        <p>
+          <a className="a-fa" href="https://www.linkedin.com/in/joshahmoors/">
+            <i className="fa fa-linkedin-square fa-2x"></i>
+          </a>
+          <a className="a-fa" href="https://github.com/joshah-moors">
+            <i className="fa fa-github fa-2x"></i>
+          </a>
+          <a className="a-fa" href="https://www.youtube.com/channel/UCN8uj2L0xk8u2o_0bvprwtg/playlists">
+            <i className="fa fa-youtube fa-2x"></i>
+          </a>
         </p>
       </div>
     )
